d48fd393121a20d567066e281eea9beb820ba649
ooda <othavio.sousa@gft.com>
2023-06-02 14:29:14 -0300

    implement create ingredient method

--- msrecipe/src/main/java/com/palmirinha/recipe/controllers/IngredientController.java
+++ msrecipe/src/main/java/com/palmirinha/recipe/controllers/IngredientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class IngredientController {
     public ResponseEntity<List<Ingredient>> getAllIngredients() {
         List<Ingredient> ingredients = ingredientService.getAllIngredients();
         return ResponseEntity.ok(ingredients);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingredient> saveIngredient(Ingredient ingredientDTO) {
+        Ingredient ingredient = ingredientService.saveIngredient(ingredientDTO);
+        return ResponseEntity.ok(ingredient);
     }
 }
