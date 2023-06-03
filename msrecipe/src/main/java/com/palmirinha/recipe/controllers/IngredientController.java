54705146a9bb8e73417bfa1fb15c556b15529bc7
ooda <othavio.sousa@gft.com>
2023-06-02 22:33:40 -0300

    Add delete and GetById methods in IngredientController

--- msrecipe/src/main/java/com/palmirinha/recipe/controllers/IngredientController.java
+++ msrecipe/src/main/java/com/palmirinha/recipe/controllers/IngredientController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +29,20 @@ public class IngredientController {
     }
 
     @PostMapping
-    public ResponseEntity<Ingredient> saveIngredient(Ingredient ingredientDTO) {
+    public ResponseEntity<Ingredient> saveIngredient(@RequestBody Ingredient ingredientDTO) {
         Ingredient ingredient = ingredientService.saveIngredient(ingredientDTO);
         return ResponseEntity.ok(ingredient);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getIngredientById(@PathVariable Long id) {
+        Ingredient ingredient = ingredientService.getIngredientById(id);
+        return ResponseEntity.ok(ingredient);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteIngredient(@PathVariable Long id) {
+        ingredientService.deleteIngredient(id);
+        return ResponseEntity.ok().build();
     }
 }
